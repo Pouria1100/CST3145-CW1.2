9fbcaf206591c8aabe87587ac0fd4f10e10dea39
Pouria1100 <94846257+Pouria1100@users.noreply.github.com>
2024-03-09 18:24:02 +0000

    Building Navbar and logo

---
+++ afterSchoolClub/src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="app">
+  <Header 
+    :siteName = "siteName"  
+    :cartLength = "cart.length" 
+    :logoImage="logoImage"  >
+
+  </Header>
+
+  <LessonView :lessons = "lessons">
+
+  </LessonView>
+
+   
+  </div>
+</template>
+
+<script>
+import LessonView from '/src/components/LessonView.vue';
+// import Checkout from '/src/components/CheckOut.vue';
+import Header from './components/Header.vue';
+
+export default {
+  name: "App",
+  data() {
+    return {
+      siteName: "After School Club",
+      logoImage: "https://images.unsplash.com/photo-1546410531-bb4caa6b424d?auto=format&fit=crop&q=80&w=2071&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      cart: [],
+      lessons: [],
+    }
+  },
+  components: {
+    Header,
+    LessonView
+  },
+  methods: {
+    // Function to fetch lessons from lessons.json file
+    fetchLessons() {
+      fetch('https://afterschool-env.eba-ri2ffiqw.eu-west-2.elasticbeanstalk.com/lessons')
+        .then(response => response.json())
+        .then(data => {
+          this.lessons = data
+
+        })
+        .catch(error => {
+          console.error('Error fetching lessons: ', error);
+        });
+    },
+  },
+  created() {
+    this.fetchLessons(); // Fetch lessons when the Vue instance is created
+  },
+}
+
+</script>
+
+<style>
+
+</style>
---
+++ afterSchoolClub/src/components/LessonView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="lessonContainer">
+     
+      <div v-for="(lesson, index) in lessons" :key="index" class="lessonCard">
+          <i v-bind:class="lesson.icon" class="lessonIcon"></i>
+          <h2>{{lesson.subject}}</h2>
+          <p>Location: {{lesson.location}}</p>
+          <p>Price: £{{lesson.price}}</p>
+          <p>Available Spaces: {{lesson.spaces}}</p>
+          <button>
+              <i v-if="lesson.spaces != 0" class="fa-solid fa-cart-plus cartIcon"></i>
+              <i v-else class="fa-solid fa-cart-shopping cartIcon"></i>
+          </button>
+      </div>
+  </div>
+  
+</template>
+
+<script>
+export default {
+  name: "LessonView",
+  props: [ "lessons"]
+   
+
+}
+</script>
+
